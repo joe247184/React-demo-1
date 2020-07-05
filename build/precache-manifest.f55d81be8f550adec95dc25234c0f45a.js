7546c26ad7ac109df1f75e366eb2b55f4513d20e
joe247184 <61569542+joe247184@users.noreply.github.com>
2020-07-05 11:11:56 +0800

    Add files via upload

---
+++ build/precache-manifest.f55d81be8f550adec95dc25234c0f45a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb9559639576bcf87442331ad9079a6f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "860cf82e6701d4d91629",
+    "url": "/static/css/main.23b5b931.chunk.css"
+  },
+  {
+    "revision": "df8ba0bde7a1f1229b38",
+    "url": "/static/js/2.9a958dab.chunk.js"
+  },
+  {
+    "revision": "860cf82e6701d4d91629",
+    "url": "/static/js/main.a4c8dcbc.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
